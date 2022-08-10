461a586f67470370b1a235891f7c1c52d5231218
jerry-Lam <imqq123456789@qq.com>
2022-08-10 16:21:12 +0800

    初始化后台项目

---
+++ admin/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="a relative">
+    <div class="b absolute top-[25px] left-[25px]"></div>
+  </div>
+</template>
+  
+<script setup lang='ts'>
+
+</script>
+  
+<style lang="postcss" scoped>
+.a {
+  width: 100px;
+  height: 100px;
+  background-color: green;
+  .b{
+    width: 50px;
+    height: 50px;
+    background-color: white;
+  }
+}
+</style>
