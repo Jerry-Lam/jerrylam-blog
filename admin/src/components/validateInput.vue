6a279b21b833178c677f067507ff623889660cfd
Jerry <jerry@DESKTOP-OJ7L2JB>
2022-09-04 20:45:06 +0800

    抽离验证器类，封装完毕

--- admin/src/components/validateInput.vue
+++ admin/src/components/validateInput.vue
@@ -1,145 +1,43 @@
 <template>
     <div>
         <label>用户名</label>
-        <input ref="userName" :dataRule="nameDataRule.maxlength = 5" v-model="nameData" type="text"
-            @change="namevalidata()">
-        <div v-if="!nameInputError">用户名格式有误</div>
+        <input :ref="userName.refDom" :dataRule="userName.dataRule = { maxlength: 5 }" v-model="userName.data.value"
+            type="text" @change="userName.validata()">
+        <div v-if="!userName.inputError.value">用户名格式有误</div>
     </div>
     <div>
         <label>密码</label>
-        <input ref="passWord" :dataRule="passwordDataRule.required = true" v-model="passwordData" type="text"
-            @change="passwordvalidata()">
-        <div v-if="!passwordInputError">密码不能为空</div>
+        <input :ref="passWord.refDom" :dataRule="passWord.dataRule = { required: true }" v-model="passWord.data.value"
+            type="text" @change="passWord.validata()">
+        <div v-if="!passWord.inputError.value">密码不能为空</div>
     </div>
 </template>
     
 <script setup lang='ts'>
-import isNumeric from '@/utils/isNumeric';
+import { Validator, type rule } from '@/utils/Validator';
 import { onMounted, ref, type Ref } from 'vue';
 
-let userName: Ref<HTMLInputElement | undefined> = ref<HTMLInputElement>();
-let passWord: Ref<HTMLInputElement | undefined> = ref<HTMLInputElement>();
+class valid {
+    refDom: Ref<HTMLInputElement | undefined> = ref<HTMLInputElement>();
+    data: Ref<string | number | undefined> = ref<string | number>();
+    inputError: Ref<boolean | undefined> = ref<boolean>(true);
+    dataRule: rule = {};
+    validate!: Validator;
 
-let nameData: Ref<string | number | undefined> = ref<string | number>();
-let passwordData: Ref<string | number | undefined> = ref<string | number>();
+    constructor() {}
 
-let nameInputError: Ref<boolean | undefined> = ref<boolean>(true);
-let passwordInputError: Ref<boolean | undefined> = ref<boolean>(true);
-
-const nameDataRule: rule = {};
-const passwordDataRule: rule = {}
-
-let namevalid: Validator;
-let passwordvalid: Validator;
+    validata() {
+        this.validate = new Validator(this.data.value, this.dataRule);
+        this.inputError.value = this.validate.is_valid();
+    }
+}
 
 onMounted(() => {
 })
 
-const namevalidata = () => {
-    namevalid = new Validator(nameData.value, nameDataRule);
-    nameInputError.value = namevalid.is_valid();
-}
+let userName = new valid();
+let passWord = new valid();
 
-const passwordvalidata = () => {
-    passwordvalid = new Validator(passwordData.value, passwordDataRule);
-    passwordInputError.value = passwordvalid.is_valid();
-    console.log(passwordInputError.value);
-}
-
-type rule = {
-    max?: number;
-    min?: number;
-    maxlength?: number,
-    minlength?: number,
-    numeric?: boolean,
-    required?: boolean
-    pattern?: string
-}
-
-class Validator {
-
-    private m_val: string | number | undefined;
-    private m_rule?: rule;
-
-    private pre_max_min() {
-        this.m_val = parseFloat(this.m_val as string);
-    }
-
-    private pre_Length() {
-        if (this.m_val)
-            this.m_val = (this.m_val as string).toString();
-    }
-
-    constructor(val: any, rule: rule) {
-        this.m_val = val;
-        this.m_rule = rule;
-    }
-
-    is_valid() {
-        let key: string;
-
-        //如果不是必填项，且没填任何内容，直接判定为合法
-        if (!this.m_rule?.required && !this.m_val)
-            return true;
-
-        for (key in this.m_rule) {
-            let r = eval(`this["Validate_${key}"]()`);
-            if (!r) return false;
-        }
-
-        return true;
-    }
-
-    Validate_max() {
-        this.pre_max_min();
-        return (this.m_val as number) <= (this.m_rule?.max as number);
-    }
-
-    Validate_min() {
-        this.pre_max_min();
-        return (this.m_val as number) <= (this.m_rule?.min as number);
-    }
-
-    Validate_maxlength() {
-        this.pre_Length();
-        if (this.m_val)
-            return (this.m_val as string).length <= (this.m_rule?.maxlength as number);
-
-        return false;
-    }
-
-    Validate_minlength() {
-        this.pre_Length();
-        if (this.m_val)
-            return (this.m_val as string).length >= (this.m_rule?.maxlength as number);
-
-        return false;
-    }
-
-    Validate_numeric() {
-        return isNumeric(this.m_val);
-    }
-
-    Validate_required() {
-        let real = (this.m_val as string).trim();
-        if (!real)
-            return false;
-        return true;
-    }
-
-    Validate_pattern() {
-        let reg = new RegExp(this.m_rule?.pattern as string);
-        //正则对象
-        return reg.test(this.m_val as string);
-    }
-
-}
-
-class input {
-    private m_rule = {};
-    private m_validator?: Validator;
-    private m_element!: Ref<HTMLInputElement | undefined>;
-}
 </script>
     
 <style>
---
+++ admin/src/utils/Validator.ts
@@ -0,0 +1,92 @@
+import isNumeric from "./isNumeric";
+
+type rule = {
+    max?: number;
+    min?: number;
+    maxlength?: number,
+    minlength?: number,
+    numeric?: boolean,
+    required?: boolean
+    pattern?: string
+}
+
+class Validator {
+
+    private m_val: string | number | undefined;
+    private m_rule?: rule;
+
+    private pre_max_min() {
+        this.m_val = parseFloat(this.m_val as string);
+    }
+
+    private pre_Length() {
+        if (this.m_val)
+            this.m_val = (this.m_val as string).toString();
+    }
+
+    constructor(val: any, rule: rule) {
+        this.m_val = val;
+        this.m_rule = rule;
+    }
+
+    is_valid() {
+        let key: string;
+
+        //如果不是必填项，且没填任何内容，直接判定为合法
+        if (!this.m_rule?.required && !this.m_val)
+            return true;
+
+        for (key in this.m_rule) {
+            let r = eval(`this["Validate_${key}"]()`);
+            if (!r) return false;
+        }
+
+        return true;
+    }
+
+    Validate_max() {
+        this.pre_max_min();
+        return (this.m_val as number) <= (this.m_rule?.max as number);
+    }
+
+    Validate_min() {
+        this.pre_max_min();
+        return (this.m_val as number) <= (this.m_rule?.min as number);
+    }
+
+    Validate_maxlength() {
+        this.pre_Length();
+        if (this.m_val)
+            return (this.m_val as string).length <= (this.m_rule?.maxlength as number);
+
+        return false;
+    }
+
+    Validate_minlength() {
+        this.pre_Length();
+        if (this.m_val)
+            return (this.m_val as string).length >= (this.m_rule?.minlength as number);
+
+        return false;
+    }
+
+    Validate_numeric() {
+        return isNumeric(this.m_val);
+    }
+
+    Validate_required() {
+        let real = (this.m_val as string).trim();
+        if (!real)
+            return false;
+        return true;
+    }
+
+    Validate_pattern() {
+        let reg = new RegExp(this.m_rule?.pattern as string);
+        //正则对象
+        return reg.test(this.m_val as string);
+    }
+
+}
+
+export { type rule, Validator };
