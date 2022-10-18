4079efc8e570a3be9214148f01738020b04c5d92
jerry-Lam <imqq123456789@qq.com>
2022-10-18 17:54:28 +0800

    update

--- admin/src/views/Dashboard.vue
+++ admin/src/views/Dashboard.vue
@@ -1,6 +1,18 @@
 <template>
-
+    <header>
+        <div class="container mx-auto bg-orange-400 p-3">
+            <label>
+                <input type="checkbox" class="peer">
+                <span class="h-4 w-4 bg-gray-200 peer-checked:hidden">
+                    123
+                </span>
+                123
+            </label>
+        </div>
+    </header>
+    <!-- header/nav/main -->
 </template>
+
     
 <script setup lang='ts'>
 
